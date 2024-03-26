792c15cbbf2b270333bf5f56e9c6755797005e2e
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2024-03-26 02:44:11 -0400

    stock view function fixes

--- mutliStockView.py
+++ mutliStockView.py
@@ -4,15 +4,47 @@ class multiStockView:
 
     def __init__(self, size):
         self.size = size #number of stocks in view
+
+        self.numStocks = 0
         self.stocks = [None for i in range(size)]
         
 
+    #removes the stock at index currIndex
+    def removeStock(self, currIndex):
+        if currIndex >= self.numStocks or currIndex < 0:
+            return
+        
+        for i in range(currIndex, self.numStocks):
+            if i == self.numStocks - 1:
+                self.stocks[i] = None
+            else:
+                self.stocks[i] = self.stocks[i+1]
 
-    def removeStock(self, i):
+        self.numStocks = self.numStocks - 1
+
         recalibrateView()
 
-    def replaceStock(self, i, newStock):
+
+    #replaces a stock at index currIndex
+    def replaceStock(self, currIndex, newStock):
+        if currIndex >= self.numStocks or currIndex < 0:
+            return
+        
+        self.stocks[currIndex] = newStock
+        
         recalibrateView()
 
+
+    #adds a stock
     def addStock(self, newStock):
+        if self.numStocks < self.size:
+            self.stocks[self.numStocks] = newStock
+            self.numStocks = self.numStocks + 1
+        else:
+            for i in range(0, self.size):
+                if i == self.size - 1:
+                    self.stocks[i] = newStock
+                else:
+                    self.stocks[i] = self.stocks[i+1]
+
         recalibrateView()
