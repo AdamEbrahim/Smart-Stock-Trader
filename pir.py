964241048f375a9e1200327c178343ccdc5f9eb4
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2024-04-21 15:57:09 -0400

    test

--- pir.py
+++ pir.py
@@ -1,18 +1,6 @@
 from sys import platform
 import time
 
-<<<<<<< HEAD
-
-if __name__ == '__main__':
-    gpio_pin = 17
-
-    pir = MotionSensor(gpio_pin)
-
-    while True:
-        pir.wait_for_active()
-        print("movement")
-        pir.wait_for_inactive()
-=======
 if platform == "linux": #If Linux (Raspberry Pi): 
     #from gpiozero import MotionSensor
     import RPi.GPIO as GPIO
@@ -30,4 +18,5 @@ def motionDetection(gpio_pin):
                 print("not any movement")
             
             time.sleep(5)
->>>>>>> 5153fa8fea34a83de85c609f43775fec58c933dc
+
+    print("hi")
